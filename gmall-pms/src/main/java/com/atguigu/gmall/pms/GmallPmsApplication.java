9b3e65daa677474924a5d1c892a63f59d9a18782
苏晓虎 <1054810745@qq.com>
2020-01-02 21:07:09 +0800

    set up gamll-pms,gmall-wms

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@MapperScan("com.atguigu.gmall.pms.dao")
+public class GmallPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallPmsApplication.class, args);
+    }
+
+}
---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/SpuInfoService.java
@@ -0,0 +1,20 @@
+package com.atguigu.gmall.pms.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.atguigu.gmall.pms.entity.SpuInfoEntity;
+import com.atguigu.core.bean.PageVo;
+import com.atguigu.core.bean.QueryCondition;
+
+
+/**
+ * spu信息
+ *
+ * @author suxiaohu
+ * @email lxf@atguigu.com
+ * @date 2020-01-02 17:13:27
+ */
+public interface SpuInfoService extends IService<SpuInfoEntity> {
+
+    PageVo queryPage(QueryCondition params);
+}
+
